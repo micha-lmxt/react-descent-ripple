2cc93231a49cead779d54edfb297c1257fe4c9b8
micha-lmxt <michael@pop-os.localdomain>
2021-01-13 16:39:03 +0100

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import ripple, { RippleParams } from 'descent-ripple';
 
-interface Props {
-  text: string
+const DescentRipple = (props: RippleParams) => {
+
+  const dref = React.useRef<HTMLDivElement>(null);
+  const [_ripple, set_ripple] = React.useState(undefined as ReturnType<typeof ripple> | undefined);
+
+  React.useEffect(() => {
+    if (dref.current && dref.current.parentNode) {
+      const rip = ripple(dref.current.parentNode, {});
+      set_ripple(rip);
+      return rip.destroy;
+    }
+    return;
+  }, []);
+
+  if (_ripple) {
+    _ripple.update(props);
+    return null;
+  }
+
+  return (
+    <div ref={dref} style={{ width: 0, height: 0, margin: 0, padding: 0, border: 0 }}></div>
+  )
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export default DescentRipple;
